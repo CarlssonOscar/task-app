6fd6cc45af493836858e31a7b9ee323094b86b5d
CarlssonOscar <oosccar@gmail.com>
2021-12-09 11:57:55 +0000

    Add Task model to models.py, enable to work in admin.py

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    tasks = models.CharField(max_length=45)
+    #Add date
+    finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.tasks

052e4169864bc68ab7026ce471ea08641627b111
CarlssonOscar <oosccar@gmail.com>
2021-12-10 08:11:57 +0000

    Enable main model to add tasks to list

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Task
 
 
 def main(request):
-    context = {}
+    the_tasks = Task.objects.order_by('id')
+    context = {'the_tasks' : the_tasks}
     return render(request, 'index.html', context)

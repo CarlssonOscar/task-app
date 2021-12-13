0a8b688b1261187a78a12ae260b4289aa74b73bc
CarlssonOscar <oosccar@gmail.com>
2021-12-13 09:17:35 +0000

    Connect base- index.html and style.css

--- taskapp/views.py
+++ taskapp/views.py
@@ -45,3 +45,4 @@ def clear(request):
 
     return redirect('main')
 
+
--- taskproject/settings.py
+++ taskproject/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['task-app007.herokuapp.com', 'localhost']
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-apricot-penguin-zisxfwem.ws-eu21.gitpod.io']
+    'https://moccasin-primate-yu2opvtd.ws-eu23.gitpod.io/']
 
 
 # Application definition
@@ -137,9 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type

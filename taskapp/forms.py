19ea0f3ad5265480e1579c425aa5132df4d616c4
CarlssonOscar <oosccar@gmail.com>
2021-12-16 15:23:33 +0000

    Add and style instructions in html

--- taskapp/forms.py
+++ taskapp/forms.py
@@ -7,8 +7,11 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['tasks']
+        widgets = {
+            'tasks': forms.TextInput(attrs={'size': '37'}),
+        }
         
     def __init__(self, *args, **kwargs): 
         super(TaskForm, self).__init__(*args, **kwargs) 
         self.fields['tasks'].label = False
-        self.fields['tasks'].widget.attrs['placeholder'] = 'Add a task'
+        self.fields['tasks'].widget.attrs['placeholder'] = 'Add a task, date, etc.'
